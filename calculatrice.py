5e6b74cd87456868b085ae6381cd3c1dd824085f
Abdelrahmane-Amiri <abdelrahmane.amiri@laplateforme.io>
2025-01-13 16:30:30 +0100

    Updated the calculator to support the '^'

--- calculatrice.py
+++ calculatrice.py
@@ -1,19 +1,19 @@
 def calculate():
-    print("Bienvenue sur la calculatrice")
-    print("Entrer un calcul")
+    print("Tapez un calcul (exemple : 12 + 34)")
 
     while True:
         enter = input(">> ")
 
         try:
-            elements = list(enter)
+            elements = enter.split()
             if len(elements) != 3:
-                print("Veuiller saisir un calcul sous forme 1+1")
+                print("Erreur : Saisir un calcul sous forme : x + y")
+                continue
 
             nombre1 = float(elements[0])
             operator = elements[1]
             nombre2 = float(elements[2])
-            
+
             match operator:
                 case "+":
                     resultat = nombre1 + nombre2
@@ -23,14 +23,19 @@ def calculate():
                     resultat = nombre1 * nombre2
                 case "/":
                     if nombre2 == 0:
-                        print("Division par 0 impossible")
+                        print("Erreur : Division par 0 impossible.")
                         continue
                     resultat = nombre1 / nombre2
-                    
+                case "^":
+                    resultat = nombre1 ** nombre2
+                case _:
+                    print(f"Erreur : Saisir un operateur valide '{operator}'.")
+                    continue
+
             print(f"Résultat : {resultat}")
-        
+
         except ValueError:
-            print("Erreur : Assurer vous d'entrer un nombre valide")
+            print("Erreur : Entrer des nombres valides.")
         except Exception as e:
             print(f"Erreur : {e}")
 
